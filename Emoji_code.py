a25cb7ea7ee18b4283ad0d54903c03f08183746b
Shreya Vaish <84989030+Shreya17vaish@users.noreply.github.com>
2022-06-28 17:15:44 +0530

    Add files via upload

---
+++ Emoji_code.py
@@ -0,0 +1,13 @@
+#Adding emojis to the text in python
+import emoji
+print(emoji.emojize("I love my Mom :Mrs._Claus:"))
+print(emoji.emojize("I have every sensitive beating_heart :beating_heart:"))
+print(emoji.emojize("I am little girl :girl:"))
+print(emoji.emojize("I am a Mums boy :boy:"))
+print(emoji.emojize("I love my Family :family:"))
+print(emoji.emojize("She there is a star :star:"))
+print(emoji.emojize("Moon :full_moon:"))
+print(emoji.emojize("water :water_wave:"))
+print(emoji.emojize("Learning python is interseting:thumbs_up:"))
+print(emoji.emojize("Today is too hot :fire:, Oh no today is too cold.:cold_face:"))
+print(emoji.emojize("I love this concept of emoji :red_heart:"))
